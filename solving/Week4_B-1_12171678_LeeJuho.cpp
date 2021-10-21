87fe437b3327f3ccf833f0b6810583d1ed82a5a0
Zzu-h <juho7785@naver.com>
2021-10-21 22:58:18 +0900

    Update

--- solving/Week4_B-1_12171678_LeeJuho.cpp
+++ solving/Week4_B-1_12171678_LeeJuho.cpp
@@ -1,49 +1,31 @@
 #include <iostream>
+#include <algorithm>
 #define INT_MAX 2147483647
 using namespace std;
-typedef pair<int, int> point;
+typedef pair<double, double> point;
 #define x first
 #define y second
-class Line {
-public:
-    int x1, y1, x2, y2;
-    Line(int x1, int  y1, int  x2, int  y2) {
-        this->x1 = y1 > y2 ? x1 : x2;
-        this->x2 = y1 < y2 ? x1 : x2;
-        this->y1 = y1 > y2 ? y1 : y2;
-        this->y2 = y1 < y2 ? y1 : y2;
-    }
-};
-int gcd(int a, int b) {
-    int c;
-    while (b != 0)
-    {
-        c = a % b;
-        a = b;
-        b = c;
-    }
-    return a;
-}
+
 point intersection(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
     int px = (x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4);
     int py = (x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4);
     int p = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-    if (p == 0) 
-        return point({ INT_MAX,INT_MAX });
-    int x = px / p;
-    int y = py / p;
+    //cout << p << ' ' << px << ' ' << py << '\n';
+    if (p == 0 && px == 0 && py == 0)
+        return point({ INT_MAX, -1}); // 같은 직선
+    else if(p == 0)
+        return point({ INT_MAX, INT_MAX }); // 평행
+    double x = px / p;
+    double y = py / p;
     return point({ x,y });
 }
 
-bool vectorCheck(int x1, int y1, int x2, int y2, int x3, int y3) {
-    // 벡터 크기와 방향이 같다면 n의 값에 따라 다륾
-    int vx1 = (x1 - x2), vx2 = (x1 - x3), vy1 = (y1 - y2), vy2 = (y1 - y3);
-    int GCD1 = gcd(abs(x1), abs(y1)) , GCD2 = gcd(abs(x2), abs(y2));
-    vx1 = x1 / GCD1; vy1 = y1 / GCD1;
-    vx2 = x2 / GCD2; vy2 = y2 / GCD2;
-    if (vx1 == vx2 && vy1 == vy2)
-        return true;
-    return false;
+bool compare(pair<point, bool>& a, pair<point, bool>& b) {
+    point first = a.first, second = b.first;
+    if (first.x == second.x) {
+        return first.y > second.y;
+    }
+    return first.x > second.x;
 }
 
 int main() {
@@ -55,22 +37,49 @@ int main() {
     while (N--) {
         int x1, y1, x2, y2, x3, y3, x4, y4, ans = 0;
         cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-        Line l1(x1, y1, x2, y2), l2(x3, y3, x4, y4);
         point p = intersection(x1, y1, x2, y2, x3, y3, x4, y4);
-        if (p.x == INT_MAX && p.y == INT_MAX) {
-            // 평행
-            if (vectorCheck(x1, y1, x2, y2, x3, y3)) {
-               // 겹침
-                if (l1.y1 > l2.y1) {
-
-                }
+        if (p.x == INT_MAX) {
+            if (p.y == INT_MAX) {
+                //평행
+                ans = 1;
             }
             else {
-                ans = 1;    // 완전한 평행
+                // 같은 직선 내에 있음
+                pair<point, bool> ar[4] = { {{x1, y1},0}, {{x2, y2},0}, {{x3, y3},1}, {{x4, y4},1} };
+                sort(ar, ar + 4, compare);
+                bool flag = ar[0].second;
+                if (ar[1].second == flag) {
+                    if (ar[1].first.x == ar[2].first.x && ar[1].first.y == ar[2].first.y)
+                        ans = 2;
+                    else
+                        ans = 1;
+                }
+                else {
+                    flag = ar[1].second;
+                    if (ar[1].first.x == ar[2].first.x && ar[1].first.y == ar[2].first.y)
+                        ans = 2;
+                    else
+                        if (flag == ar[2].second)
+                            ans = 4;
+                        else
+                            ans = 3;
+                }
             }
         }
         else {
-            // 교차점 발생
+            // 두 직선 사이에 교점이 존재 교점 비교
+            pair<point, bool> ar1[3] = { {{x1, y1},0}, {{x2, y2},0}, {{p.x, p.y },1} };
+            pair<point, bool> ar2[3] = { {{x3, y3},0}, {{x4, y4},0}, {{p.x, p.y },1} };
+            sort(ar1, ar1 + 3, compare);
+            sort(ar2, ar2 + 3, compare);
+            if (ar1[1].first.x == ar1[2].first.x && ar1[1].first.y == ar1[2].first.y && ar1[2].second)
+                swap(ar1[1], ar1[2]);
+            if (ar2[1].first.x == ar2[2].first.x && ar2[1].first.y == ar2[2].first.y && ar2[2].second)
+                swap(ar2[1], ar2[2]);
+            if(ar1[1].second && ar2[1].second)
+                ans = 2;
+            else
+                ans = 1;
         }
 
         cout << ans << '\n';
